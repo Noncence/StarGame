3a28973b29c5309ed8e24d452d95bd0da05b6885
Sergei <njbots@yandex.ru>
2020-03-30 15:16:02 +0300

    lessons 3

--- core/src/com/mygdx/game/screen/MenuScreen.java
+++ core/src/com/mygdx/game/screen/MenuScreen.java
@@ -5,34 +5,29 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.base.BaseScreen;
+import com.mygdx.game.exception.GameException;
+import com.mygdx.game.math.Rect;
+import com.mygdx.game.sprites.Background;
 
 public class MenuScreen extends BaseScreen {
-    private Texture ship, background;
-    private Vector2 pos, pos2;
-    private Vector2 v;
+    private Texture bg;
+    private Background background;
+    private Vector2 pos;
 
     @Override
     public void show() {
 
-        background = new Texture("font.jpg");
-        ship = new Texture("ship.png");
-        pos = new Vector2(5,5);
-        pos2 = new Vector2(0,0);
-        v = new Vector2(0,0);
+        bg = new Texture("font.jpg");
+        try {
+            background = new Background(bg);
+        } catch (GameException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+
+        }
+        pos = new Vector2();
         super.show();
 
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        pos2.set(screenX - ship.getWidth()/2, (Gdx.graphics.getHeight()-screenY) - ship.getHeight()/2);
-        v.set(0,0);
-        v.set(pos2.cpy().sub(pos));
-        v.nor().scl(1.5f);
-        System.out.println("v = " + v);
-        System.out.println("pos = " + pos);
-        System.out.println("pos2 = " + pos2);
-        return false;
     }
 
     @Override
@@ -44,23 +39,29 @@ public class MenuScreen extends BaseScreen {
     @Override
     public void dispose() {
         batch.dispose();
-        background.dispose();
-        ship.dispose();
+        bg.dispose();
         super.dispose();
     }
     private void update(float delta){
-       if  (pos2.len() - pos.len() >  -1.0f && pos2.len() - pos.len() < 1.0f){
-            v.set(0,0);
-            pos.set(pos2);
-       } else {
-            pos.add(v);}
+
     }
     private void draw (){
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(background, 0, 0);
-        batch.draw(ship, pos.x , pos.y );
+        background.draw(batch);
         batch.end();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        pos.set(touch);
+        return false;
+
     }
 }
